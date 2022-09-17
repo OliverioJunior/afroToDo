560b51a0b72d053862119e2958576d64f3b4b3fa
Oliverio <oliverio.junior2@gmail.com>
2022-09-17 20:42:33 -0300

    corrigir bug da margin

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -4,7 +4,8 @@ export const ContainerMain = styled.main`
   max-width: 960px;
   height: calc(100vh - 72px);
   display: flex;
-  margin: 2rem auto 0;
+  margin: 0 auto;
+  padding: 2rem 0;
   align-items: flex-start;
   justify-content: center;
 `;
